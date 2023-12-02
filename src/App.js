7e1ee4aa51712ed8e981797279fb5d2e7ee76546
Vlatko Juric <juricv78gmail.com>
2023-12-02 15:29:00 +0100

    generated some basic dummy data for the faqs,created a Accordion component ,a Accordion item component,passed in the props to the AccordionItem component,added basic jsx to the AccordionItem component,mapped out the faqs as an AccordionItem

---
+++ src/App.js
@@ -0,0 +1,43 @@
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((faq) => (
+        <AccordionItem />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  return (
+    <div className="item">
+      <p className="number">{num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">-</p>
+      <div className="content-box">{text}</div>
+    </div>
+  );
+}
