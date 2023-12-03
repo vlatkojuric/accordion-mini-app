ad5a191837bbaba52d402b5c8dfe062a217265bf
Vlatko Juric <juricv78gmail.com>
2023-12-03 17:05:08 +0100

    changed css mistake

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function AccordionItem({ num, title, text }) {
   }
 
   return (
-    <div className="item" onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
